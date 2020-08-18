b892bd056fad929a4a026fa0ee1daffef4ae66c2
Thiago Pires <thiago.pires@sectrans.com.br>
2020-08-18 14:43:56 -0300

    release - balance update

--- clientpanel/src/app/components/client-details/client-details.component.ts
+++ clientpanel/src/app/components/client-details/client-details.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ClientService } from '../../services/client.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+
+
 
 import { Client } from '../../models/Client';
 
@@ -9,12 +13,37 @@ import { Client } from '../../models/Client';
   styleUrls: ['./client-details.component.css']
 })
 export class ClientDetailsComponent implements OnInit {
-  clients: Client[];
+  id: string;
+  client: Client;
+  hasBalance: boolean = false;
+  showBalanceUpdateInput: boolean = false;
 
-  constructor(private clientservice: ClientService) { }
+  constructor(
+    private clientservice: ClientService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private flashMessage: FlashMessagesService
+    ) { }
 
   ngOnInit() {
-    this.clientservice.getClients().subscribe(clients => console.log(clients));
+    // Recuperar id da url
+    this.id = this.route.snapshot.params['id'];
+    // get client
+    this.clientservice.getClient(this.id).subscribe(client => {
+      if(client != null){
+        if (client.balance > 0){
+          this.hasBalance = true;
+        }
+      }
+      this.client = client;
+    });
+  }
+
+  updateBalance(){
+    this.clientservice.updateClient(this.client);
+    this.flashMessage.show('Renda atualizada', {
+      cssClass: 'alert-success', timeout:4000
+    });
   }
 
 }
